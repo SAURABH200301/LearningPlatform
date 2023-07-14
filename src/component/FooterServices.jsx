d4113138c9e3759ab03f68d44815d4b10964c06f
SAURABH200301 <saurabhsharma200301@gmail.com>
2023-07-14 15:13:09 +0530

    edited bit of style

--- src/component/FooterServices.jsx
+++ src/component/FooterServices.jsx
@@ -1,20 +1,24 @@
 import classes from "./FooterServices.module.css";
 
-
 function FooterServices(prop) {
   return (
-    <div className={classes.footerServices}>
-      <h3 className={classes.H3}>{prop.head}</h3>
-      <ul className={classes.ul}>
-        {prop.data.map((s) => {
-          return (
-            <li key={s.id} className={classes.Li}>
-              <div>{s.title}</div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div
+        className={`${classes.gradientBorder} ${classes.gradientColor1}`}
+      ></div>
+      <div className={classes.footerServices}>
+        <h3 className={classes.H3}>{prop.head}</h3>
+        <ul className={classes.ul}>
+          {prop.data.map((s) => {
+            return (
+              <li key={s.id} className={classes.Li}>
+                <div>{s.title}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
